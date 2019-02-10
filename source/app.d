d251c63026b877a8816d44441166bb5c4f799150
Lee Avital <leeavital@gmail.com>
2019-02-10 15:02:07 -0500

    Add usage

--- source/app.d
+++ source/app.d
@@ -19,8 +19,8 @@ int main(string[] args)
   auto result = p.parse();
 
   if (result.errors.length > 0) {
+    import errors;
     foreach(const e ; result.errors) {
-      import errors;
       formatError(stderr, q, e);
     }
     return 1;
@@ -42,5 +42,34 @@ int main(string[] args)
 }
 
 void usage() {
-  writefln("TODO: usage here");
+  import std.string;
+  auto u = `
+    esq -- a swiss army knife for elasticsearch
+
+    esq is meant to be installed on a local machine, and outputs curl commands which
+    can be piped, or easily copied into a remote session.
+
+    For example:
+
+      esq 'SELECT FROM "testindex" LIMIT 10
+
+    Will output a query to select 10 items from the index "testindex".
+
+      curl -XPOST http://localhost:9200/testindex/_search?pretty=true -H "Content-Type: application/json" -d '{"size": 10}'
+
+    You can pipe the output directly into a vagrant session:
+
+      esq 'SELECT FROM "testindex" LIMIT 10 | vagrant ssh
+
+    Or kubernetes shell:
+
+      esq 'SELECT FROM "testindex" LIMIT 10 | kubectl exec -it my-pod-name /bin/bash
+
+    Or vanilla SSH session:
+
+      esq 'SELECT FROM "testindex" LIMIT 10 | ssh my-elasticsearch-host
+    `;
+
+  write(outdent(u));
+
 }
