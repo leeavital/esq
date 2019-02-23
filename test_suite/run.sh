25c7630cfa9cb88b8fb454ce885ecddd66228f5d
Lee Avital <leeavital@gmail.com>
2019-02-23 15:47:16 -0500

    Make some changes to satisfy shellcheck

--- test_suite/run.sh
+++ test_suite/run.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-pushd "$(dirname $0)/.." > /dev/null
+pushd "$(dirname "$0")/.." > /dev/null
 dub build --build=release
 export PATH=$PATH:$PWD
 popd > /dev/null
 
-pushd "$(dirname $0)" > /dev/null
+pushd "$(dirname "$0")" > /dev/null
 dirs=$(find . -maxdepth 1 -type d -not -name '.')
 
 set +e
@@ -15,16 +15,17 @@ set +e
 for d in $dirs
 do
   echo "testing $d"
-  source $d/command.sh > $d/actual.out 2> $d/actual.err
+  # shellcheck source=/dev/null
+  source "$d/command.sh" > "$d/actual.out" 2> "$d/actual.err"
 
-  err_diff=$(diff $d/actual.err $d/expected.err)
+  err_diff=$(diff "$d/actual.err" "$d/expected.err")
   if [[ "$?" != "0" ]]
   then
     echo "stderr did not match"
     echo "$err_diff"
   fi
 
-  out_diff=$(diff $d/actual.out $d/expected.out)
+  out_diff=$(diff "$d/actual.out" "$d/expected.out")
   if [[ "$?" != "0" ]]
   then
     echo "stdout did not match"
