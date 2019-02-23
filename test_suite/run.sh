fdef66d1feac610641ab77b0e41891a8f533ab3f
Lee Avital <leeavital@gmail.com>
2019-02-23 15:40:25 -0500

    Complete run.sh, add a single test

--- test_suite/run.sh
+++ test_suite/run.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname $0)/.."
+set -e
+
+pushd "$(dirname $0)/.." > /dev/null
 dub build --build=release
 export PATH=$PATH:$PWD
-popd
+popd > /dev/null
 
-pushd "$(dirname $0)"
+pushd "$(dirname $0)" > /dev/null
 dirs=$(find . -maxdepth 1 -type d -not -name '.')
+
+set +e
 
 for d in $dirs
 do
+  echo "testing $d"
   source $d/command.sh > $d/actual.out 2> $d/actual.err
+
+  err_diff=$(diff $d/actual.err $d/expected.err)
+  if [[ "$?" != "0" ]]
+  then
+    echo "stderr did not match"
+    echo "$err_diff"
+  fi
+
+  out_diff=$(diff $d/actual.out $d/expected.out)
+  if [[ "$?" != "0" ]]
+  then
+    echo "stdout did not match"
+    echo "$out_diff"
+  fi
 done
 
-popd
+popd > /dev/null
